1b1c18c492104b63666355fc248b8d8501d161b7
Nicanor Martinez <111961688+MartinezNicanor@users.noreply.github.com>
2024-01-15 13:27:22 +0100

    Add login and logout functionality

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -7,6 +7,7 @@ import Header from './components/header/header';
 import Login from './pages/login/login';
 import ResetPassword from './pages/reset password/resetPassword';
 import UserOverview from './pages/user overview/userOverview';
+import { useAuthContext } from './components/hooks/useAuthContext';
 
 function App() {
 
@@ -16,20 +17,22 @@ function App() {
     { id: 1, Email: 'jess@hootmail.com', FirstName: 'Jess', LastName: 'Doe', Address: '123 Main St', PaymentMethod: 'Credit Card', Subscription: 'Premium', NumProfiles: 3, Status: 'Disactive' },
   ];
 
+  const { user } = useAuthContext()
+
   return (
-    <div className="main-container">
-        <Header />
-        <Router>
-            <Routes>
-                <Route path={"/"} element={<Homepage />} />
-                <Route path={"/profile"} element={<Profile />} />
-                <Route path={"/login"} element={<Login />} />
-                <Route path={"/resetPassword"} element={<ResetPassword />} />
-                <Route path={"/userOverview"} element={<UserOverview data={userData} profile="senior" />} />
-            </Routes>
-        </Router>
-        <Footer />
-    </div>
+            <Router>
+                <div className="main-container">
+                        <Header />
+                        <Routes>
+                            <Route path={"/"} element={user ? <Homepage /> : <Navigate to="/login" />} />
+                            <Route path={"/profile"} element={user ? <Profile /> : <Navigate to="/login" />} />
+                            <Route path={"/resetPassword"} element={user ? <ResetPassword /> : <Navigate to="/login" />} />
+                            <Route path={"/userOverview"} element={user ? <UserOverview data={userData} profile="senior" /> : <Navigate to="/login" />} />
+                            <Route path={"/login"} element={!user ? <Login /> : <Navigate to="/" />} />
+                        </Routes>
+                        <Footer />
+                </div>
+            </Router>
   );
 }
 
--- netflix/src/components/header/header.js
+++ netflix/src/components/header/header.js
@@ -1,16 +1,23 @@
 import logo from "../../assets/logo.png";
+import { useLogout } from "../hooks/useLogout";
 
 const Header = () => {
+    const { logout } = useLogout()
+    
+    const handleClick = () => {
+        logout()
+    }
+
     return(
         <header>
             <div className="header-links">
                 <a href="/profile">My Profile</a>
-                <a href="#">Users overview</a>
+                <a href="userOverview">Users overview</a>
             </div>
             <a href="/"><img className="header-logo" src={logo} /></a>
             <div className="header-buttons">
-                <button className="login-button">Log in</button>
-                <button className="logout-button">Log out</button>
+                <a href="/login" className="login-button">Log in</a>
+                <button className="logout-button" onClick={handleClick}>Log out</button>
             </div>
         </header>
     );
