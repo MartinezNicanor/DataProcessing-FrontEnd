c7d1c202ace06a77e476731cdd219a862143dd3d
CaterinaAresti <111961519+CaterinaAresti@users.noreply.github.com>
2024-01-20 22:37:05 +0100

    ADD: google charts

--- netflix/src/pages/home/statistics.js
+++ netflix/src/pages/home/statistics.js
@@ -1,3 +1,99 @@
+import { Chart } from "react-google-charts";
+
+export const paymentData = [
+    ["Payment Method", "Accounts"],
+    ["Mastercard", 5175000],
+    ["Visa", 4792000],
+    ["Apple Pay", 3695000],
+    ["Google Pay", 2299000],
+    ["Ideal", 1526000],
+  ];
+
+export const paymentOptions = {
+    chartArea: { width: "60%"},
+    backgroundColor: {
+            fill: "black",
+            stroke: "black",
+            strokeWidth: 1,
+        },
+    colors: ["#E50914", "#8E1212", "#F70505", "#680303", "#F15757"],
+    legend: {
+        textStyle: {
+            color: 'white',
+        },
+    },
+};
+
+export const subscriptionData = [
+    ["Subscriptions", "Accounts"],
+    ["Active", 2695000],
+    ["Inactive", 1526548],
+  ];
+
+export const subscriptionOptions = {
+    chartArea: { width: "60%"},
+    backgroundColor: {
+            fill: "black",
+            stroke: "black",
+            strokeWidth: 1,
+        },
+    colors: ["red", "gray"],
+    legend: {
+        textStyle: {
+            color: 'white',
+        },
+    },
+};
+
+export const topTenData = [
+    ["Country", "Revenue"],
+    ["USA", 9175756],
+    ["Canada", 8792865],
+    ["UK", 7695268],
+    ["China", 6896512],
+    ["Germany", 5657429],
+    ["Japan", 5099584],
+    ["Australia", 4524579],
+    ["Italy", 3657842],
+    ["Netherlands", 2598411],
+    ["Nepal", 1256994],
+];
+
+export const topTenOptions = {
+    chartArea : {width: "75%"},
+    hAxis: {
+        title: "Country",
+        textStyle: {
+            color: 'white',
+            },
+        titleTextStyle: {
+            color: 'white',
+            },
+        minValue: 0,
+    },
+    vAxis: {
+        title: "Revenue",
+        textStyle: {
+            color: 'white',
+            },
+        titleTextStyle: {
+            color: 'white',
+            },
+    },
+    backgroundColor: {
+        fill: "black",
+        stroke: "black",
+        strokeWidth: 1,
+    },
+    colors: ["#E50914", "#8E1212"],
+    legend: {
+        textStyle: {
+        color: 'white',
+        },
+    },
+};
+        
+
 const Statistics = () => {
     return(
         <div className="stats-container">
@@ -5,47 +101,45 @@ const Statistics = () => {
             <div className="tables-container">
                 <div className="stats-table">
                     <div className="stats-title">
-                        <p>Number of monthly subscriptions</p>
-                        <select name="monthly-subs">
-                            <option value="">year</option>
-                            <option value="">year</option>
-                            <option value="">year</option>
-                        </select>
+                        <p>Most Used Payment Method</p>
                     </div>
-                    <div className="stats"></div>
+                    <div className="stats">
+                        <Chart
+                            chartType="PieChart"
+                            width="100%"
+                            height="400px"
+                            data={paymentData}
+                            options={paymentOptions}
+                        />
+                    </div>
                 </div>
                 <div className="stats-table">
                     <div className="stats-title">
-                        <p>Most popular subscription type</p>
-                        <select name="monthly-subs">
-                            <option value="">year</option>
-                            <option value="">year</option>
-                            <option value="">year</option>
-                        </select>
+                        <p>Active and Inactive Subscription</p>
                     </div>
-                    <div className="stats"></div>
+                    <div className="stats">
+                        <Chart
+                            chartType="PieChart"
+                            width="100%"
+                            height="400px"
+                            data={subscriptionData}
+                            options={subscriptionOptions}
+                        />
+                    </div>
                 </div>
-                <div className="stats-table">
+                <div className="topTenStat">
                     <div className="stats-title">
-                        <p>Annaul revenue</p>
-                        <select name="monthly-subs">
-                            <option value="">year</option>
-                            <option value="">year</option>
-                            <option value="">year</option>
-                        </select>
+                        <p>Top Ten Country by Revenue</p>
                     </div>
-                    <div className="stats"></div>
-                </div>
-                <div className="stats-table">
-                    <div className="stats-title">
-                        <p>Monthly revenue</p>
-                        <select name="monthly-subs">
-                            <option value="">year</option>
-                            <option value="">year</option>
-                            <option value="">year</option>
-                        </select>
+                    <div className="stats">
+                        <Chart
+                            chartType="ColumnChart"
+                            width="100%"
+                            height="400px"
+                            data={topTenData}
+                            options={topTenOptions}
+                        />
                     </div>
-                    <div className="stats"></div>
                 </div>
             </div>
         </div>
