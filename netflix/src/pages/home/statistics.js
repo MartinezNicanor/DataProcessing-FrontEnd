4ed1e9da9a1d383a3bfca77b910aef8efe8b9471
Nicanor Martinez <111961688+MartinezNicanor@users.noreply.github.com>
2024-01-21 11:56:20 +0100

    Add useEffect for statistics

--- netflix/src/pages/home/statistics.js
+++ netflix/src/pages/home/statistics.js
@@ -1,4 +1,29 @@
+import { useEffect } from "react";
+import { useAuthContext } from "../../components/hooks/useAuthContext";
+
 const Statistics = () => {
+    const { user } = useAuthContext();
+
+    useEffect(() => {
+        const fetchStatistics = async () => {
+            const response = await fetch(`http://localhost:4000/admin/statistics`, {
+                method: 'GET',
+                headers: {'Authorization': `Bearer ${user.token}`},
+            })
+            const json = await response.json()
+            
+            if (!response.ok) {
+                console.log(json.error);
+            }
+            
+            if (response.ok) {
+                console.log(json.data);
+            }
+        }
+
+        fetchStatistics();
+    }, [user])
+
     return(
         <div className="stats-container">
             <h1 className="homepage-title">WORLD WIDE</h1>
